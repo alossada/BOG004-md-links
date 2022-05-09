86073b2abe34e6d42fe5cb3b54080104006dc465
unknown <alossada@gmail.com>
2022-05-08 19:49:36 -0500

    se organiza condicional si la ruta no es valida o si es valida

--- src/index.js
+++ src/index.js
@@ -8,54 +8,40 @@ const{
 } = require('./node-methods.js')
 
 //-----Funcion Mdlinks-----//
-const mdLinks = (path, option = {validate: false}) => new Promise ((resolve, reject) => {
-    
-    //---Capturar ruta desde los argumentos---//
-    // const pathArgument = args[2];
-    // console.log('|    ✿ PATH ARGUMENT ✿    | --->', pathArgument);
-    
-    //-----Convierte la ruta capturada en Absoluta-----//
+const mdLinks = (path, option) => new Promise ((resolve, reject) => {
+    //----Convertir la ruta a absoluta---//
     const pathArgAbsolut = convertPathToAbsolut(path);
-    console.log('|    ✿ RUTA ABSOLUTA ✿    | --->', pathArgAbsolut);
-
+    
     //----Validar si la ruta existe---//
     const validatePathRes  = validatePath(pathArgAbsolut);
-    console.log(('|    ✿ RUTA VALIDA ✿   |'), validatePathRes);
-
-    // //---Invoca funcion para obtener archivos recursivamente de la ruta capturada---//
+    
+    //--- declarar array vacio para guardar archivo .md desde getMdFiles---//
     let arrayMdFile = [];
-    if (validatePathRes=== false) {
-        console.log('| ✿ LA RUTA NO EXISTE ✿ |');
+    
+    //---condicional, si la ruta es valida obtiene  archivos .md---//
+    if (validatePathRes === false) {
+        resolve('| ✿ INVALID PATH ✿ |');
     } else if (validatePathRes){
         const filesMd = getMdFiles(arrayMdFile, pathArgAbsolut);
-        if(filesMd.length === 0){
-            console.log('|     ✿ DIRECTORIO NO CONTIENE ARCHIVOS .md ✿     |');
-        } else {
-            console.log('|    ✿ ARCHIVOS ENCONTRADOS ✿   |', filesMd);
-        }
-    }else{
-        console.log('|     ✿ LA RUTA INGRESADA NO ES VÁLIDA ✿     |');
-    };
-    
-    readFileContent(arrayMdFile)
-        .then((objectLinks) =>{
-            if(objectLinks.length === 0){
-                console.log('|     ✿ El archivo no contiene links ✿     |');
-            }else{
-                console.log('Que archivos encuentra?',objectLinks);
-                if(option.validate === true){
+        if (filesMd.length === 0){
+            resolve('| ✿ EMPTY DIRECTORY ✿ |')
+        }else{
+            readFileContent(arrayMdFile)
+            .then((objectLinks) =>{
+                if(objectLinks.length === 0){
+                console.log('|   ✿ EMPTY FILE ✿   |');
+                }else{
+                    if(option.validate === true){
                     httpPetition(objectLinks).then(response => {
                         resolve(response)
                     })
-                } else {
+                    } else {
                     resolve(objectLinks);
+                    }
                 }
-            }
-        })        
-        .catch((error) => {
-            const errorMessage = 'Error';
-            reject(error, errorMessage) 
-        });
+            })        
+        }
+    }
 });
 
 
