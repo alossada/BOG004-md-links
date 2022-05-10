cd8f173b418761f6043cb637bcf2557a763169e9
unknown <alossada@gmail.com>
2022-05-10 17:44:52 -0500

    Correcciones de identacion

--- src/index.js
+++ src/index.js
@@ -1,47 +1,41 @@
-//-----Importamos Modulos de Node----//
-const{
-    convertPathToAbsolut,
-    validatePath,
-    getMdFiles,
-    readFileContent,
-    httpPetition,
-} = require('./node-methods.js')
+// -----Importamos Modulos de Node----//
+// eslint-disable-next-line import/extensions
+const {convertPathToAbsolut, validatePath, getMdFiles, readFileContent, httpPetition} = require('./node-methods');
+// -----Funcion Mdlinks-----//
+const mdLinks = (path, option = {validate: false}) => new Promise((resolve, reject) => {  
+//----Convertir la ruta a absoluta---//
+  const pathArgAbsolut = convertPathToAbsolut(path);
 
-//-----Funcion Mdlinks-----//
-const mdLinks = (path, option) => new Promise ((resolve, reject) => {
-    //----Convertir la ruta a absoluta---//
-    const pathArgAbsolut = convertPathToAbsolut(path);
-    
-    //----Validar si la ruta existe---//
-    const validatePathRes  = validatePath(pathArgAbsolut);
-    
-    //--- declarar array vacio para guardar archivo .md desde getMdFiles---//
-    let arrayMdFile = [];
-    
-    //---condicional, si la ruta es valida obtiene  archivos .md---//
-    if (validatePathRes === false) {
-        resolve('| ✿ INVALID PATH ✿ |');
-    } else if (validatePathRes){
-        const filesMd = getMdFiles(arrayMdFile, pathArgAbsolut);
-        if (filesMd.length === 0){
-            resolve('| ✿ EMPTY DIRECTORY ✿ |')
-        }else{
-            readFileContent(arrayMdFile)
-            .then((objectLinks) =>{
-                if(objectLinks.length === 0){
-                console.log('|   ✿ EMPTY FILE ✿   |');
-                }else{
-                    if(option.validate === true){
-                    httpPetition(objectLinks).then(response => {
-                        resolve(response)
-                    })
-                    } else {
-                    resolve(objectLinks);
-                    }
-                }
-            })        
-        }
+// ----Validar si la ruta existe---//
+const validatePathRes = validatePath(pathArgAbsolut);
+
+// --- declarar array vacio para guardar archivo .md desde getMdFiles---//
+let arrayMdFile = [];
+
+//---condicional, si la ruta es valida obtiene  archivos .md---//
+if (validatePathRes === false) {
+    reject('| ✿ INVALID PATH ✿ |');
+} else if (validatePathRes) {
+    const filesMd = getMdFiles(arrayMdFile, pathArgAbsolut); //---Funcion recursiva que revisa el directorio--//
+    if (filesMd.length === 0) { //---Si el directorio es vacio---//
+      reject('| ✿ EMPTY DIRECTORY ✿ |');
+    } else {
+      readFileContent(arrayMdFile)
+        .then((objectLinks) => {
+          if (objectLinks.length === 0) { //---Si el documento no tiene links---//
+            reject('|   ✿ EMPTY FILE ✿   |');
+          } else {
+            if (option.validate === true) {
+              httpPetition(objectLinks).then(response => {
+                resolve(response)
+              })
+            } else {
+              resolve(objectLinks);
+            }
+          }
+        })
     }
+  }
 });
 
 
