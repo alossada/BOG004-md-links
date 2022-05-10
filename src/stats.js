c8ca5deec23e5aa02a6b434ba67f7700f797cea0
unknown <alossada@gmail.com>
2022-05-10 17:46:03 -0500

    e crea funcion que muestra total de links y links unicos

--- src/stats.js
+++ src/stats.js
@@ -1,37 +1,24 @@
 const chalk = require('chalk');
 
-//---FUNCION PARA OBTENER LA CANTIDAD TOTAL DE LINKS UNICOS---//
-const uniqueLinks = (arrObj) => {
-    const arrUniqueLinks = [];
-    const arrUrls = [];
-    arrObj.forEach((obj) => arrUrls.push(obj.href));
-    arrUrls.forEach((url) => {
-    if (!arrUniqueLinks.includes(url)) {
-        arrUniqueLinks.push(url);
-    }
-});
-    return arrUniqueLinks.length;
-};
-
 const arrayTemplate = (arrayLinks) => {
-    console.log(chalk.magenta.bold(`\n |     ✿ FOUND lINKS: ✿    | \n`));
+    console.log(chalk.magenta.bold(`\n |     ✿ ALL LINKS: ✿    | \n`));
     arrayLinks.forEach(link => {
         console.log(chalk.yellowBright.bold(` ▷ href:  ${chalk.cyan(link.href)}   \n ▷ text:  ${chalk.magenta(link.text)} \n ▷ fileName: ${chalk.blackBright(link.fileName)} \n ❀`));
     })
 };
 
-const statusTemplate = (arrayLinks) => {
+const statusTemplate = (arrayLinks) => { //--VALIDATE
     console.log(chalk.magenta.bold(`| ✿ LINK STATUS: ✿ |`));
     arrayLinks.forEach(link => {
         if (link.status === 200) {
             console.log(chalk.yellowBright.bold(`▷ href: ${chalk.cyan(link.href)} \n▷ status: ${chalk.cyanBright.bold(link.status)} \n▷ ok: ${chalk.magentaBright.bold(link.ok)}\n `));
         } else {
-            console.log(chalk.yellowBright.bold(`▷ href: ${chalk.cyan(link.href)} \n▷ status: ${chalk.cyanBright.bold(link.status)} \n▷ fail: ${chalk.redBright.bold(link.fail)}\n ❀ \n`));
+            console.log(chalk.yellowBright.bold(`▷ href: ${chalk.cyan(link.href)} \n▷ status: ${chalk.cyanBright.bold(link.status)} \n▷ fail: ${chalk.redBright.bold(link.ok)}\n ❀ \n`));
         }
     })
 }
 
-const totalLinks = (arraylinks) => {
+const totalLinks = (arraylinks) => { // --STATS
     const totalArray = arraylinks.map(link => link.href);
     const uniqueLinks = [...new Set(totalArray)];
     const brokenLinks = arraylinks.filter(link => link.status != 200)
@@ -42,9 +29,19 @@ const totalLinks = (arraylinks) => {
     `
 };
 
+const linkStats = (arraylinks) => { // --STATS
+    const totalArray = arraylinks.map(link => link.href);
+    const uniqueLinks = [...new Set(totalArray)];
+    return `${chalk.magenta.bold(`
+    |  ✿   STATS  ✿  |
+            `)}
+    ${chalk.yellowBright.bold(`\t▷ Total:${totalArray.length} \n\t▷ Unique:${uniqueLinks.length}\n\t`)}
+    `
+};
+
 module.exports = {
     arrayTemplate,
     statusTemplate,
     totalLinks,
-    uniqueLinks,
+    linkStats,
 }
