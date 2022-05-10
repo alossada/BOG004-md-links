478b860042aa3230125f055fe30adf6bc4edec25
unknown <alossada@gmail.com>
2022-05-10 18:57:12 -0500

    NUeva configuracion de reglas de Eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,22 @@ module.exports = {
   env: {
     browser: true,
     commonjs: true,
+    node: true,
     es2021: true,
+    'jest/globals': true,
   },
-  // extends: [
-  //   'airbnb-base',
-  // ],
+  extends: [
+    'airbnb-base',
+  ],
   parserOptions: {
     ecmaVersion: 'latest',
   },
   rules: {
+    'no-console': 'off',
   },
+  plugins: [
+    'eslint-plugin-jest',
+  ],
+
 };
+
